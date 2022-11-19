8aa982ec3226cf4869e6e235af241d4a713b7d1d
unknown <you@example.com>
2022-11-19 09:16:55 +0530

    Added : established connection

--- src/main/java/com/bridgelabz/EmployeePayroll_JDBC.java
+++ src/main/java/com/bridgelabz/EmployeePayroll_JDBC.java
@@ -1,8 +1,8 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 import static java.sql.DriverManager.getConnection;
 
@@ -13,18 +13,30 @@ public class EmployeePayroll_JDBC {
     String dbUsername = "root";
     String dbPassword = "Ullas@8105897193";
 
+    ArrayList<EmployeeData> employeeDataArrayList = new ArrayList<>();
     public static void main(String[] args) {
         EmployeePayroll_JDBC employeePayroll_jdbc = new EmployeePayroll_JDBC();
         employeePayroll_jdbc.connectDatabase();
+        EmployeePayroll_JDBC.listDrivers();
 
     }
 
     public void connectDatabase() {
         try {
+            System.out.println("Connecnting to database");
             connection = getConnection(dbUrl, dbUsername, dbPassword);
             statement = connection.createStatement();
+            System.out.println("Connecnting to database is successful");
         } catch (SQLException e) {
             throw new UserException(UserException.ExceptionType.SQLException,"SQL Query Not Excuted Properly.");
+        }
+    }
+
+    private static void listDrivers(){
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass = (Driver)driverList.nextElement();
+            System.out.println("" + driverClass.getClass().getName());
         }
     }
 }
